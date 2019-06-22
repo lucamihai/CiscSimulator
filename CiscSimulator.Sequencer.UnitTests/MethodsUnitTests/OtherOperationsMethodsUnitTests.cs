a1c138701f5c30933272418d15380818eb7c7ecf
Mihai Luca <luca.mihai97@yahoo.com>
2019-06-23 01:22:26 +0300

    Partially implemented OtherOperationsMethods

---
+++ CiscSimulator.Sequencer.UnitTests/MethodsUnitTests/OtherOperationsMethodsUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using CiscSimulator.Sequencer.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CiscSimulator.Sequencer.UnitTests.MethodsUnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class OtherOperationsMethodsUnitTests
+    {
+        private Sequencer sequencer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sequencer = new Sequencer();
+            sequencer.ProgramCounterRegister.Data.Value = 10;
+            sequencer.StackPointerRegister.Data.Value = 10;
+        }
+
+        [TestMethod]
+        public void IncrementPcIncrementsProgramCounterRegisterValue()
+        {
+            var valueBeforeIncrement = sequencer.ProgramCounterRegister.Data.Value;
+
+            OtherOperationsMethods.IncrementPc(sequencer);
+
+            Assert.IsTrue(sequencer.ProgramCounterRegister.Data.Value == valueBeforeIncrement + 1);
+        }
+
+        [TestMethod]
+        public void IncrementSpIncrementsStackPointerRegisterValue()
+        {
+            var valueBeforeIncrement = sequencer.StackPointerRegister.Data.Value;
+
+            OtherOperationsMethods.IncrementSp(sequencer);
+
+            Assert.IsTrue(sequencer.StackPointerRegister.Data.Value == valueBeforeIncrement + 1);
+        }
+
+        [TestMethod]
+        public void DecrementSpDecrementsStackPointerRegisterValue()
+        {
+            var valueBeforeDecrement = sequencer.StackPointerRegister.Data.Value;
+
+            OtherOperationsMethods.DecrementSp(sequencer);
+
+            Assert.IsTrue(sequencer.StackPointerRegister.Data.Value == valueBeforeDecrement - 1);
+        }
+    }
+}
---
+++ CiscSimulator.Sequencer/Methods/OtherOperationsMethods.cs
@@ -0,0 +1,20 @@
+﻿namespace CiscSimulator.Sequencer.Methods
+{
+    public static class OtherOperationsMethods
+    {
+        public static void IncrementPc(Sequencer sequencer)
+        {
+            sequencer.ProgramCounterRegister.Data.Value++;
+        }
+
+        public static void IncrementSp(Sequencer sequencer)
+        {
+            sequencer.StackPointerRegister.Data.Value++;
+        }
+
+        public static void DecrementSp(Sequencer sequencer)
+        {
+            sequencer.StackPointerRegister.Data.Value--;
+        }
+    }
+}
