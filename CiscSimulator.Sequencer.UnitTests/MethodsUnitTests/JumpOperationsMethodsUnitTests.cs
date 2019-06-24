f7f42cf86a6fb7103f936a2a723193436f2d937c
lucamihai <luca.mihai97@yahoo.com>
2019-06-24 11:19:15 +0300

    Added JumpOperationsMethods skeleton

---
+++ CiscSimulator.Sequencer.UnitTests/MethodsUnitTests/JumpOperationsMethodsUnitTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using CiscSimulator.Sequencer.Methods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CiscSimulator.Sequencer.UnitTests.MethodsUnitTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class JumpOperationsMethodsUnitTests
+    {
+        private Sequencer sequencer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sequencer = new Sequencer();
+        }
+
+        [TestMethod]
+        public void StepIncrementsMpmAddressRegisterValue()
+        {
+            var mpmAddressRegisterValueBeforeStep = sequencer.MpmAddressRegister.Data.Value;
+            JumpOperationsMethods.Step(sequencer);
+            var mpmAddressRegisterValueAfterStep = sequencer.MpmAddressRegister.Data.Value;
+
+            Assert.AreEqual(mpmAddressRegisterValueBeforeStep + 1, mpmAddressRegisterValueAfterStep);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void JumpThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.Jump(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void JumpIThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.JumpI(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void B1ThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.B1(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void AdThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.Ad(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void ZThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.Z(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void CThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.C(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void VThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.V(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void SThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.S(sequencer);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void IntrThrowsNotImplementedException()
+        {
+            JumpOperationsMethods.Intr(sequencer);
+        }
+    }
+}
---
+++ CiscSimulator.Sequencer/Methods/JumpOperationsMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace CiscSimulator.Sequencer.Methods
+{
+    public static class JumpOperationsMethods
+    {
+        public static void Step(Sequencer sequencer)
+        {
+            sequencer.MpmAddressRegister.Data.Value++;
+        }
+
+        public static void Jump(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void JumpI(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void B1(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Ad(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Z(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void C(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void V(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void S(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Intr(Sequencer sequencer)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
